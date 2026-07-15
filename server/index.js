c7ceede763f75a36b995a5fd9e9e7c0b7bc27db3
Abhishek <dxabhi8@gmail.com>
2026-07-15 23:22:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://interview-iq-1-sc1e.onrender.com",
     credentials:true
 }))
 
